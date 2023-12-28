a86451aa874e9a04b00575338ec3a8a542f5ae22
Rick <rickblaas@live.nl>
2023-12-28 12:49:23 +0100

    de user kan nu een password reset doen :)

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements CanResetPassword
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -49,6 +50,6 @@ class User extends Authenticatable
         return $this->hasOne(Profile::class);
     }
 
-    
-    
+
+
 }
